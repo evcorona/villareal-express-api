998bfdde7e454a021654cd2651a8b969ab433a78
evcorona <eldav.corona@gmail.com>
2023-04-13 14:14:47 -0600

    🛂 Add login and signup features

---
+++ src/routes/auth.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const auth = require('./../usecases/auth')
+
+const router = express.Router()
+
+router.post('/signup', async (req, res) => {
+  try {
+    await auth.signup(req.body)
+
+    res.json({
+      success: true,
+      message: 'User created successfully',
+    })
+  } catch (error) {
+    res.status(400)
+    res.json({ success: false, message: error.message })
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const token = await auth.login(req.body)
+
+    res.json({
+      success: true,
+      message: 'User logged in successfully',
+      data: { token },
+    })
+  } catch (error) {
+    res.status(401)
+    res.json({ success: false, message: error.message })
+  }
+})
+
+module.exports = router
---
+++ src/usecases/auth.js
@@ -0,0 +1,42 @@
+const User = require('./../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+async function signup(credentials) {
+  const { password, ...userData } = credentials
+
+  const passwordEncrypted = await bcrypt.hash(password, 10)
+
+  return User.create({ password: passwordEncrypted, userData })
+}
+
+async function login(credentials) {
+  const { email, password } = credentials
+
+  const user = await User.findOne({ email })
+  if (!user) throw new Error('Invalid credentials')
+
+  const isValid = await bcrypt.compare(password, user.password)
+  if (!isValid) throw new Error('Invalid credentials')
+
+  await User.findByIdAndUpdate(user._id, { lastLoginAt: new Date() })
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET
+  )
+
+  return token
+}
+
+async function updateUser({ _id, ...userData }) {
+  return await User.findByIdAndUpdate(_id, userData, {
+    returnDocument: 'after',
+  })
+}
+
+module.exports = {
+  signup,
+  login,
+  updateUser,
+}
