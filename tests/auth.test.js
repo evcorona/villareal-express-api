ad1f71ba332046b13e917cabe8283feee2347070
evcorona <eldav.corona@gmail.com>
2023-04-14 18:48:16 -0600

    🧪 Add sign up tests

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -4,6 +4,7 @@ const request = require('supertest')
 require('dotenv').config()
 
 const app = require('../src/server')
+const { loginAdmin, loginUser } = require('./utils/loginProcess')
 
 const { DB_TEST_URI, TEST_USER, TEST_PASSWORD } = process.env
 
@@ -25,13 +26,49 @@ describe('PUSH /auth ', () => {
     expect(response.statusCode).toBe(200)
   })
 
-  test('Check if a invalid user can not login', async () => {
+  test('Check if an invalid user can not log in', async () => {
     const response = await await request(app).post('/auth/login').send({
       email: 'wrong-email@test.com',
       password: 'wrong-password',
     })
 
     expect(response.body.message).toEqual('Invalid credentials')
+    expect(response.statusCode).toBe(401)
+  })
+
+  test('Check signup process', async () => {
+    const adminToken = await loginAdmin(app)
+
+    const response = await await request(app)
+      .post('/auth/signup')
+      .set('Authorization', adminToken)
+      .send({
+        email: TEST_USER,
+        password: TEST_PASSWORD,
+        role: 'cobranza',
+        firstName: 'Test',
+        lastName: 'Testing',
+      })
+
+    expect(response.body.message).toContain('duplicate key error')
+    expect(response.statusCode).toBe(400)
+  })
+
+  test('Check if signup is restricted for common user', async () => {
+    const userToken = await loginUser(app)
+
+    const response = await await request(app)
+      .post('/auth/signup')
+      .set('Authorization', userToken)
+      .send({
+        email: TEST_USER,
+        password: TEST_PASSWORD,
+        role: 'cobranza',
+        firstName: 'Test',
+        lastName: 'Testing',
+      })
+
+    expect(response.body.message).toContain('Unauthorized')
     expect(response.statusCode).toBe(401)
   })
 })
