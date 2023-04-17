702e54ca5882383181ac7d20b03e15f21d21eeae
evcorona <eldav.corona@gmail.com>
2023-04-17 16:45:24 -0600

    Avoid repeated registers

--- src/usecases/house.js
+++ src/usecases/house.js
@@ -1,6 +1,11 @@
-const House = require('./../models/Income')
+const House = require('./../models/House')
 
 async function addHouse(houseData) {
+  const { street, houseNumber } = houseData
+
+  const isRepeated = await House.findOne({ street, houseNumber })
+  if (isRepeated) throw new Error('House already exists')
+
   const newHouse = await House.create(houseData)
 
   return newHouse
